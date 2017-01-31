9aa4c2955d93af61ba370485b2609e96f69cfba3
Addono <a.d.knapen@student.tue.nl>
2017-01-31 21:17:52 +0100

    config/refactor: switched to new .nl url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://boerenkoolfuif.ga';
+$config['base_url'] = 'http://boerenkoolfuif.nl';
 
 /*
 |--------------------------------------------------------------------------
