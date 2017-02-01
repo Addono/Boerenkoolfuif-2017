961f3f57828c3a8503d83ef2ac8d03d697a8f415
Addono <a.d.knapen@student.tue.nl>
2017-02-01 02:54:35 +0100

    install/controller: added install for receipt entries

--- application/controllers/Install.php
+++ application/controllers/Install.php
@@ -7,26 +7,59 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Install extends CI_Controller {
 
+    private $usersTable = 'users_table';
+    private $receiptsEntries = 'receipts_entries';
+
     public function index() {
         $this->load->database();
         $this->load->dbforge();
 
-        $this->addTable('users_table', $this->getUsersTableFields());
+        $this->addTable($this->usersTable, $this->getUsersTableFields());
+        $this->addTable($this->receiptsEntries, $this->getReceiptEntriesTable());
     }
 
     private function addTable($name, $fields, $attr = ['ENGINE' => 'InnoDB']) {
         if($this->db->table_exists($name)) {
-            echo "Table '$name' already exists.\n";
+            echo "Table '$name' already exists.<br>";
         } else {
             $this->dbforge->add_field($fields);
             $this->dbforge->add_field('id');
             if($this->dbforge->create_table($name, TRUE, $attr)) {
-                echo "Succesfully added table '$name'\n";
+                echo "Succesfully added table '$name'<br>";
             } else {
-                echo "Failed adding table '$name'";
+                echo "Failed adding table '$name'<br>";
                 exit;
             }
         }
+    }
+
+    private function getReceiptEntriesTable() {
+        return [
+            'group_id' => [
+                'type' => 'INT',
+                'constraint' => '9',
+            ],
+            'resource0' => [
+                'type' => 'TINYINT',
+                'constraint' => 1,
+            ],
+            'resource1' => [
+                'type' => 'TINYINT',
+                'constraint' => 1,
+            ],
+            'resource2' => [
+                'type' => 'TINYINT',
+                'constraint' => 1,
+            ],
+            'score' => [
+                'type' => 'TINYINT',
+                'constraint' => 1,
+            ],
+            'country' => [
+                'type' => 'ENUM("netherlands","belgium","france","germany")',
+            ],
+            'FOREIGN KEY (group_id) REFERENCES '.$this->usersTable.'(id)',
+        ];
     }
 
     private function getUsersTableFields() {
